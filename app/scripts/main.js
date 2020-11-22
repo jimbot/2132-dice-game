63d8e1127f4295fb795b45c022f436290c87dc60
James Chen <jameschen1295@gmail.com>
2020-11-22 13:12:33 -0800

    import dice images

---
+++ app/scripts/main.js
@@ -0,0 +1,5 @@
+/* Constants */
+const player_die_one = $('div#player div.dice_pair_one span').text();
+const player_die_two = $('div#player div.dice_pair_two span').text();
+const computer_die_one = $('div#computer div.dice_pair_one span').text();
+const computer_die_two = $('div#computer div.dice_pair_two span').text();
